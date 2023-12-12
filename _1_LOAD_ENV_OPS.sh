27b193fbe8ecfef90320f6ee8be608635c3a0b3c
congnq_nexlesoft <congnq@nexlesoft.com>
2023-12-12 11:36:47 +0700

    add composer command 2

--- _1_LOAD_ENV_OPS.sh
+++ _1_LOAD_ENV_OPS.sh
@@ -25,21 +25,21 @@ if [ "${BRANCH}" = "develop" ]; then
   export API_DEPLOY_BRANCH=develop-multi-container
   export EB_ENVIRONMENT_NAME="develop-multi-container"
   #
-  export COMPOSER_COMMAND="composer update"
+  export COMPOSER_UPDATE="composer update"
 fi
 if [ "${BRANCH}" = "staging" ]; then
   export ENV=stg
   export API_DEPLOY_BRANCH=staging-multi-container
   export EB_ENVIRONMENT_NAME="staging-multi-container"
   #
-  export COMPOSER_COMMAND="composer update --no-dev --optimize-autoloader --no-scripts"
+  export COMPOSER_UPDATE="composer update --no-dev --optimize-autoloader --no-scripts"
 fi
 if [ "${BRANCH}" = "master" ]; then
   export ENV=prd
   export API_DEPLOY_BRANCH=master-multi-container
   export EB_ENVIRONMENT_NAME="engageplus-prod-multi-container"
   #
-  export COMPOSER_COMMAND="composer update --no-dev --optimize-autoloader --no-scripts"
+  export COMPOSER_UPDATE="composer update --no-dev --optimize-autoloader --no-scripts"
 fi
 # === END ===
 
