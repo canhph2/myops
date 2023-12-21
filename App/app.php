a7c4a44e5d02a635d770eb02dd875c8d053e6f0d
congnq_nexlesoft <congnq@nexlesoft.com>
2023-12-21 17:03:09 +0700

    release OPS SHARED LIBRARY (PHP) v2.0.75 on 2023-12-21 10:03:09 UTC

--- App/app.php
+++ App/app.php
@@ -30,7 +30,7 @@ class App
      * 2.1: with test lib before ship
      * @var string
      */
-    const APP_VERSION = '2.0.74';
+    const APP_VERSION = '2.0.75';
 
     const SHELL_HANDLE_ENV_OPS_DATA_BASE64 = '';
 
