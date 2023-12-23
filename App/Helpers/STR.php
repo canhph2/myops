abe122c87d81e7de517d85239d32a48b1fae54bc
congnq_nexlesoft <congnq@nexlesoft.com>
2023-12-23 19:09:32 +0700

    add STR helper

---
+++ App/Helpers/STR.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Helpers;
+
+/**
+ * this is a simple STRing helper for PHP < 8.1
+ */
+class STR
+{
+    /**
+     * @param string $toCheck
+     * @param string $search
+     * @return bool
+     */
+    public static function contains(string $toCheck, string $search): bool
+    {
+        return strpos($toCheck, $search) !== false;
+    }
+
+    /**
+     * @param string $toCheck
+     * @param string $search
+     * @return bool
+     */
+    public static function startWith(string $toCheck, string $search): bool
+    {
+        return strpos($toCheck, $search) === 0;
+    }
+
+    /**
+     * @param string $toCheck
+     * @param string $search
+     * @return bool
+     */
+    public static function endWith(string $toCheck, string $search): bool
+    {
+        $length = strlen($search);
+        if ($length === 0) {
+            return false; // Empty needle always matches
+        }
+        return substr($toCheck, -$length) === $search;
+    }
+}
