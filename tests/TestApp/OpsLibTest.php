ff2b67671b8a19710c46c46270015ed8ea20af3e
congnq_nexlesoft <congnq@nexlesoft.com>
2024-04-11 17:29:31 +0700

    release [34;1mOPS APP (PHP) v3.0.19[0m on 2024-04-11 10:29:31 UTC

--- tests/TestApp/OpsLibTest.php
+++ tests/TestApp/OpsLibTest.php
@@ -12,6 +12,7 @@ require_once 'app/Enum/IconEnum.php';
 require_once 'app/Helpers/DirHelper.php';
 require_once 'app/Helpers/StrHelper.php';
 require_once 'app/Helpers/UIHelper.php';
+require_once 'app/Traits/ConsoleUITrait.php';
 //
 require_once 'tests/TestApp/BaseTestCase.php';
 
