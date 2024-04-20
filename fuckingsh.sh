17fa622092820dee0e3f31e87a65cc25e2ac77b1
congnq_nexlesoft <congnq@nexlesoft.com>
2024-04-20 21:22:25 +0700

    release [34;1mOPS APP (PHP) v3.2.14[0m on 2024-04-20 14:22:24 UTC

---
+++ fuckingsh.sh
@@ -0,0 +1,10 @@
+# way 1  | load shell confiugration
+#source ~/.zshrc1 || true
+#source ~/.bashrc || true
+
+# load shell configuration
+[[ -f ~/.zshrc ]] && source ~/.zshrc # MAC
+[[ -f ~/.bashrc ]] && source ~/.bashrc # Ubuntu
+
+
+ops-app version
