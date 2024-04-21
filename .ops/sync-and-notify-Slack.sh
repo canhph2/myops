3c393eb153200bb7872c8dfa16fb62d3ec078b12
congnq_nexlesoft <congnq@nexlesoft.com>
2024-04-21 21:24:21 +0700

    sync

--- .ops/sync-and-notify-Slack.sh
+++ .ops/sync-and-notify-Slack.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e # tells the shell to exit if a command returns a non-zero exit status
 # [Alias required] load shell configuration
-#[[ -f ~/.zshrc ]] && source ~/.zshrc # MAC
-#[[ -f ~/.bashrc ]] && source ~/.bashrc # Ubuntu
+[[ -f ~/.zshrc ]] && source ~/.zshrc # MAC
+[[ -f ~/.bashrc ]] && source ~/.bashrc # Ubuntu
 
 # usage:    sh _ops/build-and-deploy-with-caches.sh 'DEVICE_NAME'
-
-#[[ -f ~/.bashrc ]] && cat ~/.bashrc # Ubuntu
-
-#. ~/.bashrc # source
-echo "The script is being run by user: $USER"
-# test system alias
-l
 
 myops version
 
