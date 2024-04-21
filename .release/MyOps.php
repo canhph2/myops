510e5c475aba2babcacfa1d7152e7ec52e49b2a6
congnq_nexlesoft <congnq@nexlesoft.com>
2024-04-21 21:26:52 +0700

    Test feature branch | Release MyOps v3.2.53 on 2024-04-21 14:26:47 UTC

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -1,5 +1,5 @@
 <?php
-// === MyOps v3.2.52 ===
+// === MyOps v3.2.53 ===
 
 // === Generated libraries classes ===
 
@@ -1268,7 +1268,7 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.2.52';
+    const APP_VERSION = '3.2.53';
 }
 
 // [REMOVED] namespace App\Enum;
