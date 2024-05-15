4dbf7c2f817c8a093f549a9fbd1b4b72afec511b
congnq_nexlesoft <congnq@nexlesoft.com>
2024-05-15 17:16:43 +0700

    fix error in command 'build-all-projects' when call myops at any where 6

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -259,7 +259,7 @@ class Release
     }
 
     /**
-     * remove tab 
+     * remove tab
      * remove namespace
      * remove some unused elements
      * @param string $classPath
@@ -2516,7 +2516,7 @@ class GitHubHelper
         }
         // handle
         //    notify
-        SlackService::sendMessageInternal(sprintf("[BEGIN] %s", CommandEnum::SUPPORT_COMMANDS()[CommandEnum::BUILD_ALL_PROJECTS][0]), DirHelper::getProjectDirName(), $branchToBuild);
+        SlackService::sendMessageInternal(sprintf("[BEGIN] %s", CommandEnum::SUPPORT_COMMANDS()[CommandEnum::BUILD_ALL_PROJECTS][0]), basename($workspaceDir), $branchToBuild);
         //    get GitHub token and login gh
         self::LineNew()->printSubTitle("login gh (GitHub CLI)");
         (new Process("login gh (GitHub CLI)", DirHelper::getWorkingDir(), [
@@ -2563,7 +2563,7 @@ class GitHubHelper
             }
         } // end loop
         //    notify
-        SlackService::sendMessageInternal(sprintf("[END] %s", CommandEnum::SUPPORT_COMMANDS()[CommandEnum::BUILD_ALL_PROJECTS][0]), DirHelper::getProjectDirName(), $branchToBuild);
+        SlackService::sendMessageInternal(sprintf("[END] %s", CommandEnum::SUPPORT_COMMANDS()[CommandEnum::BUILD_ALL_PROJECTS][0]), basename($workspaceDir), $branchToBuild);
     }
 
 
