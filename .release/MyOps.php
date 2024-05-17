d1cace65afd363765b9231455726610b23205004
congnq_nexlesoft <congnq@nexlesoft.com>
2024-05-17 14:00:21 +0700

    T | Release MyOps v3.6.20 on 2024-05-17 07:00:20 UTC

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -1,5 +1,5 @@
 <?php
-// === MyOps v3.6.19 ===
+// === MyOps v3.6.20 ===
 
 // === Generated libraries classes ===
 
@@ -1480,7 +1480,7 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.6.19';
+    const APP_VERSION = '3.6.20';
 }
 
 // [REMOVED] namespace App\Enum;
