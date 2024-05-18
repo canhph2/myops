aea461889511197fb1f0ecb84813e88f0d0e9cd0
congnq_nexlesoft <congnq@nexlesoft.com>
2024-05-18 18:47:43 +0700

    Support field value arguments `--field1=value1` | Release MyOps v3.7.0 on 2024-05-18 11:47:25 UTC

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -1,5 +1,5 @@
 <?php
-// === MyOps v3.6.20 ===
+// === MyOps v3.7.0 ===
 
 // === Generated libraries classes ===
 
@@ -1480,7 +1480,7 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.6.20';
+    const APP_VERSION = '3.7.0';
 }
 
 // [REMOVED] namespace App\Enum;
