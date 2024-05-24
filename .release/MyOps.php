8a8e3785c1d8730701149c27f78c5591db5e7746
congnq_nexlesoft <congnq@nexlesoft.com>
2024-05-24 09:53:15 +0700

    Fix bug myops helper | Release MyOps v3.9.3 on 2024-05-24 02:53:12 UTC

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -1,5 +1,5 @@
 <?php
-// === MyOps v3.9.2 ===
+// === MyOps v3.9.3 ===
 
 // === Generated libraries classes ===
 
@@ -1616,7 +1616,7 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.9.2';
+    const APP_VERSION = '3.9.3';
 }
 
 // [REMOVED] namespace App\Enum;
