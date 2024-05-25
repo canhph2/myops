8a436430ab5d98171262fb05dc38247d558b2670
congnq_nexlesoft <congnq@nexlesoft.com>
2024-05-25 22:54:38 +0700

    add a CI/CD script file build all backend

--- .ops/build-all-backend-and-deploy-ELB.sh
+++ .ops/build-all-backend-and-deploy-ELB.sh
@@ -4,16 +4,17 @@ set -e # tells the shell to exit if a command returns a non-zero exit status
 [[ -f ~/.zshrc ]] && source ~/.zshrc # MAC
 [[ -f ~/.bashrc ]] && source ~/.bashrc # Ubuntu
 
-# usage:    sh .ops/sync-and-notify-Slack.sh 'DEVICE_NAME'
+# usage:    sh .ops/build-all-backend-and-deploy-ELB.sh
 
 echo "todo build all backend"
 myops branch
 exit 0;
+
+# cleanup: in case success, in case failure and exit with code at any commands
+trap 'myops post-work --type=finish --process-id=${PROCESS_ID} --exit-code=$? ' EXIT
 #
-eval "$(php app/MyOps.php pre-work --response-type=eval)"
-php app/MyOps.php pre-work
-# validate
-php app/MyOps.php validate --type=device --type=branch
+eval "$(myops pre-work --response-type=eval)"
+myops pre-work --type=start
 # handle
 php app/MyOps.php sync
 php app/MyOps.php slack --message="${DEVICE} just synced $(myops version no-format-color) successfully" \
