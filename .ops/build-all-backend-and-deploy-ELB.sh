31daa0a00d429730e570635eff6c3006708449db
congnq_nexlesoft <congnq@nexlesoft.com>
2024-05-25 23:49:01 +0700

    add completed script to build all backend

--- .ops/build-all-backend-and-deploy-ELB.sh
+++ .ops/build-all-backend-and-deploy-ELB.sh
@@ -4,19 +4,39 @@ set -e # tells the shell to exit if a command returns a non-zero exit status
 [[ -f ~/.zshrc ]] && source ~/.zshrc # MAC
 [[ -f ~/.bashrc ]] && source ~/.bashrc # Ubuntu
 
-# usage:    sh .ops/build-all-backend-and-deploy-ELB.sh
-
-echo "todo build all backend"
-myops branch
-exit 0;
+# usage:    sh .ops/build-all-backend-and-deploy-ELB.sh 'DEVICE_NAME'
 
 # cleanup: in case success, in case failure and exit with code at any commands
-trap 'myops post-work --type=finish --process-id=${PROCESS_ID} --exit-code=$? ' EXIT
+trap 'myops post-work --process-id=${PROCESS_ID} --exit-code=$?    --message="${DEVICE} just finished deploying all backend projects" ' EXIT
 #
 eval "$(myops pre-work --response-type=eval)"
-myops pre-work --type=start
+myops pre-work --message="${DEVICE} starts to build all backend projects:"
+# validate
+myops validate --type=device --type=branch --type=docker
 # handle
-php app/MyOps.php sync
-php app/MyOps.php slack --message="${DEVICE} just synced $(myops version no-format-color) successfully" \
-                        --process-id=${PROCESS_ID} --exit-code=$?
+#    Build Docker images
+#        API module
+myops checkout-caches engage-api-deploy ${API_DEPLOY_BRANCH}
+cd "${ENGAGEPLUS_CACHES_DIR}/engage-api-deploy"
+chmod u+x ".ops/build-api-docker-image-and-push-to-ECR.sh" && . ".ops/build-api-docker-image-and-push-to-ECR.sh"
+myops slack --message="-> just finished building Docker image of Admin API and Booking API :heavy_check_mark:"
+#        Invoice service
+myops checkout-caches invoice-service ${BRANCH}
+cd "${ENGAGEPLUS_CACHES_DIR}/invoice-service"
+chmod u+x ".ops/build-docker-image-and-push-to-ECR.sh" && . ".ops/build-docker-image-and-push-to-ECR.sh"
+myops slack --message="-> just finished building Docker image of Invoice Service :heavy_check_mark:"
+#        Payment service
+myops checkout-caches payment-service ${BRANCH}
+cd "${ENGAGEPLUS_CACHES_DIR}/payment-service"
+chmod u+x ".ops/build-docker-image-and-push-to-ECR.sh" && . ".ops/build-docker-image-and-push-to-ECR.sh"
+myops slack --message="-> just finished building Docker image of Payment Service :heavy_check_mark:"
+#        Integration API
+myops checkout-caches integration-api ${BRANCH}
+cd "${ENGAGEPLUS_CACHES_DIR}/integration-api"
+chmod u+x ".ops/build-docker-image-and-push-to-ECR.sh" && . ".ops/build-docker-image-and-push-to-ECR.sh"
+myops slack --message="-> just finished building Docker image of Integration API :heavy_check_mark:"
+#    Deploy ELB
+myops slack --message="-> starts to deploying new version to ELB (Elastic Beanstalk) :arrows_counterclockwise:"
+myops elb-update-version
+
 
