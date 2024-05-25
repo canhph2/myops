e95682b3f36907a847f534c9f8529ffdbbe0b9b9
congnq_nexlesoft <congnq@nexlesoft.com>
2024-05-25 23:21:21 +0700

    test 16

--- .ops/build-all-backend-and-deploy-ELB.sh
+++ .ops/build-all-backend-and-deploy-ELB.sh
@@ -7,11 +7,10 @@ set -e # tells the shell to exit if a command returns a non-zero exit status
 # usage:    sh .ops/build-all-backend-and-deploy-ELB.sh
 
 # cleanup: in case success, in case failure and exit with code at any commands
-POST_WORK_COMMAND="myops post-work --type=finish --process-id=${PROCESS_ID} --exit-code=$?"
-trap 'myops post-work --type=finish --process-id=${PROCESS_ID} --exit-code=$? ' EXIT
+trap 'myops post-work --type=finish --process-id=${PROCESS_ID} --exit-code=$?    --message="${DEVICE} just finished abcd" ' EXIT
 #
 eval "$(myops pre-work --response-type=eval)"
-myops pre-work --type=start
+myops pre-work --message="${DEVICE} starts to build all backend projects"
 # validate
 myops validate --type=device --type=branch --type=docker
 # handle
