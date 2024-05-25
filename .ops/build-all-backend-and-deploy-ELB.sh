c644a3d918feb8c6a91897233542b2206c78948a
congnq_nexlesoft <congnq@nexlesoft.com>
2024-05-25 23:12:47 +0700

    test 12

--- .ops/build-all-backend-and-deploy-ELB.sh
+++ .ops/build-all-backend-and-deploy-ELB.sh
@@ -6,10 +6,6 @@ set -e # tells the shell to exit if a command returns a non-zero exit status
 
 # usage:    sh .ops/build-all-backend-and-deploy-ELB.sh
 
-echo "todo build all backend"
-myops branch
-exit 0;
-
 # cleanup: in case success, in case failure and exit with code at any commands
 POST_WORK_COMMAND="myops post-work --type=finish --process-id=${PROCESS_ID} --exit-code=$?"
 trap '${POST_WORK_COMMAND}' EXIT
