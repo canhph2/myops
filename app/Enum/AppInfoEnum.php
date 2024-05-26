784812bad20ed4884760da890590053a97b2912e
congnq_nexlesoft <congnq@nexlesoft.com>
2024-05-26 23:33:55 +0700

    Release MyOps v3.12.35 on 2024-05-26 16:33:52 UTC

--- app/Enum/AppInfoEnum.php
+++ app/Enum/AppInfoEnum.php
@@ -7,5 +7,5 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.12.34';
+    const APP_VERSION = '3.12.35';
 }
