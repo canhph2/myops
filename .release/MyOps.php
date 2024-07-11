3a5b1306d37b0aef95b2daa5bf7c5161cf9a869b
congnq_nexlesoft <congnq@nexlesoft.com>
2024-07-11 15:18:52 +0700

    Add OS info to info command | Release MyOps v3.14.11 on 2024-07-11 08:18:41 UTC

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -1,5 +1,5 @@
 <?php
-// === MyOps v3.14.10 ===
+// === MyOps v3.14.11 ===
 
 // === Generated libraries classes ===
 
@@ -1639,7 +1639,7 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.14.10';
+    const APP_VERSION = '3.14.11';
 }
 
 // [REMOVED] namespace App\Enum;
@@ -2203,6 +2203,7 @@ class GitHubFactory
 // [REMOVED] namespace App\Helpers;
 
 // [REMOVED] use App\Classes\Base\CustomCollection;
+// [REMOVED] use App\Classes\Process;
 // [REMOVED] use App\Enum\DevelopmentEnum;
 // [REMOVED] use App\Enum\IconEnum;
 // [REMOVED] use App\Enum\IndentLevelEnum;
@@ -2227,52 +2228,80 @@ class AppInfoHelper
         }
     }
 
-    public static function info(): void {
+    public static function info(): void
+    {
+        self::lineNew()->printTitle("project info");
+        self::lineIcon(IconEnum::DOT)->print('OS               : %s', self::color(self::getOSVersion(), UIEnum::COLOR_BLUE));
         // validate
         $envPath = DirHelper::getWorkingDir(DevelopmentEnum::DOT_CONFIG_RYT);
-        if(!is_file($envPath)){
-            self::lineTagMultiple(TagEnum::VALIDATION_ERROR)->print('%s not found',DevelopmentEnum::DOT_CONFIG_RYT);
+        if (!is_file($envPath)) {
+            self::lineTagMultiple(TagEnum::VALIDATION_ERROR)->print('%s not found', DevelopmentEnum::DOT_CONFIG_RYT);
             return; // END
         }
         // handle
         //    get env value
         $tempEnvs = new CustomCollection(StrHelper::findLinesContainsTextInFile($envPath, 'APP_NAME'));
-        if(!getenv('SYS_ENV')){
+        if (!getenv('SYS_ENV')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'SYS_ENV'));
         }
-        if(!getenv('FRONTEND_URL')){
+        if (!getenv('FRONTEND_URL')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'FRONTEND_URL'));
         }
-        if(!getenv('DB_HOST')){
+        if (!getenv('DB_HOST')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'DB_HOST'));
         }
-        if(!getenv('DB_DATABASE_API')){
+        if (!getenv('DB_DATABASE_API')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'DB_DATABASE_API'));
         }
-        if(!getenv('DB_DATABASE_PAYMENT')){
+        if (!getenv('DB_DATABASE_PAYMENT')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'DB_DATABASE_PAYMENT'));
         }
         foreach ($tempEnvs as $line) {
             putenv($line);
         }
         //    print
-        self::lineNew()->printTitle("project info");
-        //    app name
+        //        app name
         $appNameFormat = self::colorFormat(ucwords(str_replace('-', ' ', getenv("APP_NAME"))),
             UIEnum::COLOR_BLUE, UIEnum::FORMAT_BOLD);
         self::lineIcon(IconEnum::DOT)->print('app name         : %s', $appNameFormat);
-        //    env
+        //        env
         self::lineIcon(IconEnum::DOT)->print('env              : %s', self::color(ENVHelper::getENVText(), UIEnum::COLOR_BLUE));
-        //    url
+        //        url
         self::lineNew()->printSeparatorLine();
-        self::lineIcon(IconEnum::DOT)->print('frontend URL     : %s',  self::color(getenv("FRONTEND_URL"), UIEnum::COLOR_BLUE));
-        //    database info
+        self::lineIcon(IconEnum::DOT)->print('frontend URL     : %s', self::color(getenv("FRONTEND_URL"), UIEnum::COLOR_BLUE));
+        //        database info
         self::lineNew()->printSeparatorLine();
-        self::lineIcon(IconEnum::DOT)->print('database host    : %s',  self::color(getenv("DB_HOST"), UIEnum::COLOR_BLUE));
-        self::lineIcon(IconEnum::DOT)->print('database API     : %s',  self::color(getenv("DB_DATABASE_API"), UIEnum::COLOR_BLUE));
-        self::lineIcon(IconEnum::DOT)->print('database payment : %s',  self::color(getenv("DB_DATABASE_PAYMENT"), UIEnum::COLOR_BLUE));
+        self::lineIcon(IconEnum::DOT)->print('database host    : %s', self::color(getenv("DB_HOST"), UIEnum::COLOR_BLUE));
+        self::lineIcon(IconEnum::DOT)->print('database API     : %s', self::color(getenv("DB_DATABASE_API"), UIEnum::COLOR_BLUE));
+        self::lineIcon(IconEnum::DOT)->print('database payment : %s', self::color(getenv("DB_DATABASE_PAYMENT"), UIEnum::COLOR_BLUE));
         //
         self::lineIndent(IndentLevelEnum::DECREASE)->printSeparatorLine();
+    }
+
+    /**
+     * @return string
+     */
+    private static function getOSVersion(): string
+    {
+        // All Linux OS
+        $LinuxOSReleaseInfoPath = '/etc/os-release';
+        if (is_file($LinuxOSReleaseInfoPath)) {
+            foreach (explode("\n", file_get_contents($LinuxOSReleaseInfoPath)) as $line) {
+                putenv($line);
+            }
+            return sprintf("%s v%s", getenv("NAME"), getenv("VERSION_ID")); // END
+        }
+        // Mac
+        $macVersionInfo = (new Process('get mac version info', DirHelper::getWorkingDir(),
+            ['sw_vers']))->execMultiInWorkDirAndGetOutputStrAll();
+        if ($macVersionInfo) {
+            foreach (explode(PHP_EOL, str_replace([':', ' ', "\t"], ['=', '', ''], $macVersionInfo)) as $line) {
+                putenv($line);
+            }
+            return sprintf("%s v%s", getenv("ProductName"), getenv("ProductVersion")); // END
+        }
+        // others
+        return 'error v0.0';
     }
 }
 
--- app/Helpers/AppInfoHelper.php
+++ app/Helpers/AppInfoHelper.php
@@ -3,6 +3,7 @@
 namespace App\Helpers;
 
 use App\Classes\Base\CustomCollection;
+use App\Classes\Process;
 use App\Enum\DevelopmentEnum;
 use App\Enum\IconEnum;
 use App\Enum\IndentLevelEnum;
@@ -27,51 +28,79 @@ class AppInfoHelper
         }
     }
 
-    public static function info(): void {
+    public static function info(): void
+    {
+        self::lineNew()->printTitle("project info");
+        self::lineIcon(IconEnum::DOT)->print('OS               : %s', self::color(self::getOSVersion(), UIEnum::COLOR_BLUE));
         // validate
         $envPath = DirHelper::getWorkingDir(DevelopmentEnum::DOT_CONFIG_RYT);
-        if(!is_file($envPath)){
-            self::lineTagMultiple(TagEnum::VALIDATION_ERROR)->print('%s not found',DevelopmentEnum::DOT_CONFIG_RYT);
+        if (!is_file($envPath)) {
+            self::lineTagMultiple(TagEnum::VALIDATION_ERROR)->print('%s not found', DevelopmentEnum::DOT_CONFIG_RYT);
             return; // END
         }
         // handle
         //    get env value
         $tempEnvs = new CustomCollection(StrHelper::findLinesContainsTextInFile($envPath, 'APP_NAME'));
-        if(!getenv('SYS_ENV')){
+        if (!getenv('SYS_ENV')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'SYS_ENV'));
         }
-        if(!getenv('FRONTEND_URL')){
+        if (!getenv('FRONTEND_URL')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'FRONTEND_URL'));
         }
-        if(!getenv('DB_HOST')){
+        if (!getenv('DB_HOST')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'DB_HOST'));
         }
-        if(!getenv('DB_DATABASE_API')){
+        if (!getenv('DB_DATABASE_API')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'DB_DATABASE_API'));
         }
-        if(!getenv('DB_DATABASE_PAYMENT')){
+        if (!getenv('DB_DATABASE_PAYMENT')) {
             $tempEnvs->merge(StrHelper::findLinesContainsTextInFile($envPath, 'DB_DATABASE_PAYMENT'));
         }
         foreach ($tempEnvs as $line) {
             putenv($line);
         }
         //    print
-        self::lineNew()->printTitle("project info");
-        //    app name
+        //        app name
         $appNameFormat = self::colorFormat(ucwords(str_replace('-', ' ', getenv("APP_NAME"))),
             UIEnum::COLOR_BLUE, UIEnum::FORMAT_BOLD);
         self::lineIcon(IconEnum::DOT)->print('app name         : %s', $appNameFormat);
-        //    env
+        //        env
         self::lineIcon(IconEnum::DOT)->print('env              : %s', self::color(ENVHelper::getENVText(), UIEnum::COLOR_BLUE));
-        //    url
+        //        url
         self::lineNew()->printSeparatorLine();
-        self::lineIcon(IconEnum::DOT)->print('frontend URL     : %s',  self::color(getenv("FRONTEND_URL"), UIEnum::COLOR_BLUE));
-        //    database info
+        self::lineIcon(IconEnum::DOT)->print('frontend URL     : %s', self::color(getenv("FRONTEND_URL"), UIEnum::COLOR_BLUE));
+        //        database info
         self::lineNew()->printSeparatorLine();
-        self::lineIcon(IconEnum::DOT)->print('database host    : %s',  self::color(getenv("DB_HOST"), UIEnum::COLOR_BLUE));
-        self::lineIcon(IconEnum::DOT)->print('database API     : %s',  self::color(getenv("DB_DATABASE_API"), UIEnum::COLOR_BLUE));
-        self::lineIcon(IconEnum::DOT)->print('database payment : %s',  self::color(getenv("DB_DATABASE_PAYMENT"), UIEnum::COLOR_BLUE));
+        self::lineIcon(IconEnum::DOT)->print('database host    : %s', self::color(getenv("DB_HOST"), UIEnum::COLOR_BLUE));
+        self::lineIcon(IconEnum::DOT)->print('database API     : %s', self::color(getenv("DB_DATABASE_API"), UIEnum::COLOR_BLUE));
+        self::lineIcon(IconEnum::DOT)->print('database payment : %s', self::color(getenv("DB_DATABASE_PAYMENT"), UIEnum::COLOR_BLUE));
         //
         self::lineIndent(IndentLevelEnum::DECREASE)->printSeparatorLine();
+    }
+
+    /**
+     * @return string
+     */
+    private static function getOSVersion(): string
+    {
+        // All Linux OS
+        $LinuxOSReleaseInfoPath = '/etc/os-release';
+        if (is_file($LinuxOSReleaseInfoPath)) {
+            foreach (explode("\n", file_get_contents($LinuxOSReleaseInfoPath)) as $line) {
+                putenv($line);
+            }
+            return sprintf("%s v%s", getenv("NAME"), getenv("VERSION_ID")); // END
+        }
+        // Mac
+        $macVersionInfo = (new Process('get mac version info', DirHelper::getWorkingDir(),
+            ['sw_vers']))->execMultiInWorkDirAndGetOutputStrAll();
+        if ($macVersionInfo) {
+            foreach (explode(PHP_EOL, str_replace([':', ' ', "\t"], ['=', '', ''], $macVersionInfo)) as $line) {
+                putenv($line);
+            }
+            return sprintf("%s v%s", getenv("ProductName"), getenv("ProductVersion")); // END
+        }
+        // others
+        return 'error v0.0';
     }
 }
