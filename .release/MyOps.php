e403d12dae68ae015df33b6578e131ee602d7908
congnq_nexlesoft <congnq@nexlesoft.com>
2024-07-22 09:20:07 +0700

    Use 3.14  | Release MyOps v3.14.19 on 2024-07-22 02:20:02 UTC

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -1,5 +1,5 @@
 <?php
-// === MyOps v3.14.18 ===
+// === MyOps v3.14.19 ===
 
 // === Generated libraries classes ===
 
@@ -1639,7 +1639,7 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.14.18';
+    const APP_VERSION = '3.14.19';
 }
 
 // [REMOVED] namespace App\Enum;
