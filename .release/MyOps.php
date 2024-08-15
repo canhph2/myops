2139a52d0b56635ebdc3f571468275037547aae7
congnq_nexlesoft <congnq@nexlesoft.com>
2024-08-15 13:18:37 +0700

    Draft 2 shared file commands | Release MyOps v3.16.1 on 2024-08-15 06:18:29 UTC

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -1,5 +1,5 @@
 <?php
-// === MyOps v3.16.0 ===
+// === MyOps v3.16.1 ===
 
 // === Generated libraries classes ===
 
@@ -1639,7 +1639,7 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.16.0';
+    const APP_VERSION = '3.16.1';
 }
 
 // [REMOVED] namespace App\Enum;
