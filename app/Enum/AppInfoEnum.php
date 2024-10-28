6447d3300b8c8699138a92bd5b56c031d00adcc4
Canh Pham <canhph2@nexlesoft.com>
2024-10-28 10:38:21 +0700

    Es1 | Release MyOps v3.17.3 on 2024-10-28 03:38:14 UTC

--- app/Enum/AppInfoEnum.php
+++ app/Enum/AppInfoEnum.php
@@ -7,5 +7,5 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.17.2';
+    const APP_VERSION = '3.17.3';
 }
