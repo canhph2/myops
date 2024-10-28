c7461d310a485296c43c650268f67f1ac6c6bd6a
congnq_nexlesoft <congnq@nexlesoft.com>
2024-10-28 10:30:08 +0700

    Test update | Release MyOps v3.17.1 on 2024-10-28 03:30:06 UTC

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -1,5 +1,5 @@
 <?php
-// === MyOps v3.17.0 ===
+// === MyOps v3.17.1 ===
 
 // === Generated libraries classes ===
 
@@ -1691,7 +1691,7 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.17.0';
+    const APP_VERSION = '3.17.1';
 }
 
 // [REMOVED] namespace App\Enum;
