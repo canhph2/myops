6d5783567073da6fbe886cf6b7b44868ad61a9ab
Canh Pham <canhph2@nexlesoft.com>
2026-05-08 14:19:07 +0700

    [#system maintenance] github action config: force using node 24 on checkout | Release MyOps v3.17.28 on 2026-05-08 07:18:45 UTC

--- .release/MyOps.php
+++ .release/MyOps.php
@@ -1,5 +1,5 @@
 <?php
-// === MyOps v3.17.27 ===
+// === MyOps v3.17.28 ===
 
 // === Generated libraries classes ===
 
@@ -1691,7 +1691,7 @@ class AppInfoEnum
     const APP_NAME = 'MyOps';
     const APP_MAIN_COMMAND = 'myops';
     const RELEASE_PATH = '.release/MyOps.php';
-    const APP_VERSION = '3.17.27';
+    const APP_VERSION = '3.17.28';
 }
 
 // [REMOVED] namespace App\Enum;
